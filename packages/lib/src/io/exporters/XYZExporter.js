cde94fa1efbf6f0641f76c8e5b1608627106f280
Boris Kozlov <kozlov.99260422@gmail.com>
2020-07-27 03:40:08 +0300

    Update XYZExporter.js

--- packages/lib/src/io/exporters/XYZExporter.js
+++ packages/lib/src/io/exporters/XYZExporter.js
@@ -1,10 +1,6 @@
 import Exporter from './Exporter';
 import Complex from '../../chem/Complex';
 
-function complexIdentifier(compound) {
-  return compound.metadata.id || compound.metadata.name || 'Unknown';
-}
-
 export default class XYZExporter extends Exporter {
   exportSync() {
     const source = this._source;
@@ -12,7 +8,7 @@ export default class XYZExporter extends Exporter {
       return null;
     }
     const numOfAtoms = source._atoms.length;
-    const result = [numOfAtoms, complexIdentifier(source)];
+    const result = [numOfAtoms, source.metadata.id || source.metadata.name || 'Unknown'];
     const atoms = this._source._atoms;
     for (let i = 0; i < numOfAtoms; i++) {
       const curAtomName = atoms[i].element.name.padEnd(6);
